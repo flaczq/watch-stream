785000ed9ec621be1df5ff6523aff58c8122bbdb
flaczq <flaczq@op.pl>
2016-03-06 15:55:54 +0100

    Developing StreamAcitivity. Trying to show stream in webView.

--- src/main/java/com/flaq/apps/watchsteam/activities/StreamActivity.java
+++ src/main/java/com/flaq/apps/watchsteam/activities/StreamActivity.java
@@ -1,9 +1,12 @@
 package com.flaq.apps.watchsteam.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import com.flaq.apps.watchsteam.R;
@@ -11,6 +14,7 @@ import com.flaq.apps.watchsteam.R;
 public class StreamActivity extends AppCompatActivity {
 
     private Intent intent;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +24,30 @@ public class StreamActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        webView = (WebView) findViewById(R.id.webView);
+
         intent = getIntent();
 
-        String name = intent.getStringExtra("stream");
-        String url = getString(R.string.url_stream_1) + name + getString(R.string.url_stream_2);
+        playStream();
+    }
 
-        Toast.makeText(StreamActivity.this, name, Toast.LENGTH_LONG).show();
+    private void playStream() {
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setSupportZoom(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                Toast.makeText(getApplicationContext(), "Wait", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        String stream = getString(R.string.url_stream_1) + intent.getStringExtra("stream") + getString(R.string.url_stream_2);
+        webView.loadUrl(stream);
     }
 
 }
